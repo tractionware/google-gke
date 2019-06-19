25bfa6b7720e55eb8afd958b49e3fc536ea3994f
David Staheli <dastahel@microsoft.com>
2019-06-19 06:57:40 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,13 +12,20 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
 apt-get update
 apt-get install -y kubectl
+echo "Roger 1"
 kubectl version
 
 echo "Deploying..."
 echo "${GCLOUD_KEY_FILE}" | base64 --decode > gcloud.json
+echo "Roger 2"
 gcloud auth activate-service-account "${GCLOUD_SERVICE_ACCOUNT}" --key-file=gcloud.json
+echo "Roger 3"
 gcloud config set project "${GCLOUD_PROJECT}"
+echo "Roger 4"
 gcloud config set compute/zone "${GCLOUD_ZONE}"
+echo "Roger 5"
 gcloud container clusters get-credentials "${GCLOUD_KUBERNETES_CLUSTER}"
+echo "Roger 6"
 kubectl set image deployments/"${GCLOUD_KUBE_SERVICE_NAME}" "${GCLOUD_KUBE_SERVICE_NAME}"="${CONTAINER_IMAGE_NAME}"
+echo "Roger 7"
 kubectl patch deployment "${GCLOUD_KUBE_SERVICE_NAME}" -p "{\"spec\":{\"template\":{\"metadata\":{\"labels\":{\"date\":\"`date +'%s'`\"}}}}}"
