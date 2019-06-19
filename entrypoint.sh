1414b826429374973be6ac4196468a5283f6cfbc
David Staheli <dastahel@microsoft.com>
2019-06-19 06:46:56 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,11 @@ curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key --keyring /
 apt-get update && apt-get install -y google-cloud-sdk
 
 echo "Installing kubectl..."
-gcloud components install kubectl
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | tee -a /etc/apt/sources.list.d/kubernetes.list
+apt-get update
+apt-get install -y kubectl
 kubectl version
 
 echo "Deploying..."
